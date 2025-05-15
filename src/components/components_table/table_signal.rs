2dde00205b3c8de0d3a497c87bba00bb53c63547
lilidm <lightdemon17@gmail.com>
2025-05-15 20:51:44 +0700

    Fix: logic if page less more than actual must be -1 page

--- src/components/components_table/table_signal.rs
+++ src/components/components_table/table_signal.rs
@@ -52,7 +52,7 @@ where
         let sort = sort.to_owned();
         let start = current_page * page.items_per_page;
         let end = start + items_per_page;
-        async move {
+        let resualt = async move {
             value.to_owned()(
                 start,
                 end,
@@ -62,7 +62,9 @@ where
                 ),
             )
             .await
-        }
+        };
+        
+        resualt
     }));
 
     use_effect(use_reactive!(|(data_resource)| {
@@ -70,14 +72,19 @@ where
             if let Some((prop_data, total_items)) = &*ref_data {
                 state.prop_data.set(prop_data.to_owned());
                 state.page_state.write().total_items = *total_items;
-                state.is_loading.set(false);
+                let total_pages = (total_items + items_per_page.saturating_sub(1))
+                    / items_per_page;
+                if current_page >= total_pages {
+                    state.page_state.write().current_page = total_pages.saturating_sub(1);
+                }
+                
             } else {
                 println!("No data available");
                 state.prop_data.set(PropData {
                     data_vec: Vec::<T>::new(),
                 });
                 state.page_state.write().total_items = 0;
-                state.is_loading.set(false);
+                
             }
         } else {
             println!("Failed to read data resource");
@@ -87,6 +94,8 @@ where
             state.page_state.write().total_items = 0;
             // Keep loading state true if we failed to read
         }
+
+        state.is_loading.set(false);
     }));
 
     use_context_provider(|| data_resource);
