64e10a72af5e91bf402627c3f1c314ba1555edc1
lidm0707 <lightdemon17@gmail.com>
2025-12-31 01:30:48 +0700

    update example

--- examples/demo_table/src/main.rs
+++ examples/demo_table/src/main.rs
@@ -68,9 +68,9 @@ fn App() -> Element {
     // let checkbox = use_checkbox::<Person>();
     // println!("{:?}", checkbox);
     rsx! {
-        // document::Stylesheet {
-        //     href: asset!("/assets/output.css"),
-        // }
+        document::Stylesheet {
+            href: asset!("/assets/output.css"),
+        }
 
         ToastContainer {
             Modal {
